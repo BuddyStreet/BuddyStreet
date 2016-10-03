78236a3c2081bfc2a6629a6266ff53b3c24d3492
FullStackPro <steve.decker@fullstackpro.com>
2016-10-02 19:52:38 -0700

    Created BuddyStreet.Web under UI/Web

---
+++ BuddyStreet.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuddyStreet.Web.MvcApplication" Language="C#" %>
---
+++ BuddyStreet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuddyStreet.Web.Startup))]
+namespace BuddyStreet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
